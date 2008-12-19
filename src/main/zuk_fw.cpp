ac23f6f90e5d0cb56f74991cec839f0f6ae0b66a
auv <kamasamikon@gmail.com>
2008-12-19 15:54:44 +0800

    remove junk file frontend.cpp

--- src/main/zuk_fw.cpp
+++ src/main/zuk_fw.cpp
@@ -1,17 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <zuk_main.h>
+#if defined(__WIN32__)
+#include <windows.h>
+#endif
 
-/**
- * @brief main
- *
- * @return 0: normal quit, 1: normal reboot
- */
+#include <QWaitCondition>
+#include <QMutex>
+#include <stdio.h>
+
+#include <QtGui/QApplication>
+#include <QTextCodec>
+
+#include <ktypes.h>
+#include <ksal.h>
+#include <kdbg.h>
+
+#include <kim.h>
+#include <kini.h>
+
+#if defined(__UNIX__)
+#include <QX11Info>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+#endif
+
+#include "zuk_main.h"
+
 int main(int argc, char **argv)
 {
+    int retval;
+
+    printf("QApplication app(argc, argv);\n");
+    QApplication app(argc, argv);
+
+    printf("zuk_init(argc, argv);\n");
     zuk_init(argc, argv);
+
+    printf("retval = app.exec();\n");
+    retval = app.exec();
+
+    printf("zuk_final(argc, argv);\n");
     zuk_final(argc, argv);
-    return 0;
+
+    printf("main:: return:%d\n", retval);
+    return retval;
 }
 
